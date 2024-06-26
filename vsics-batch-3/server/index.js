971ad4f5fcd70afc48bbef42e64404f35c813e5e
debasish <devopsadmin@kloudzcomputing.tech>
2024-06-26 23:09:20 +0530

    Batch 2 -Batch 3

---
+++ vsics-batch-3/server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+
+const connect = require("./databases/mongo.connect");
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 6000;
+const HOST = "http://localhost:";
+
+const corsOptions = {
+  origin: ["http://localhost:6000", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsOptions));
+
+app.get("/", async (req, res) => {
+  res.send("API is running...");
+});
+
+connect();
+
+app.listen(PORT, console.log(`Server running on port ${HOST}${PORT}`));
